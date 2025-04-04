64ca18087f07a21e480085952edc4d5f5179da75
Vansh Tyagi <vanshjii021@gmail.com>
2025-04-04 20:16:03 +0530

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -25,11 +25,7 @@ console.log('Environment Variables Loaded:', {
 // Rest object
 const app = express();
 
-// CORS configuration
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-})); 
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -45,10 +41,16 @@ app.set('view engine', 'ejs');
 connectDb();
 
 //middlewares
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(
+  cors({
+      origin: (origin, callback) => {
+          callback(null, true); // Allow all origins
+      },
+      credentials: true,
+  })
+);
+
+
 app.use(morgan('dev'))
 
 //routes
